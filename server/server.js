1a2097debacc770a07957f157bf632ff8db0c754
master <011999gv@gmail.com>
2020-07-01 02:40:45 +0530

    Add geolocation via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,33 +3,35 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user Connected');
     
     socket.on('disconnect',()=>{
         console.log('User was Disconnected')
     });
+
     socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
+
     socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined'));
+
     socket.on('createMessage',(message,callback)=>{
         console.log('CreateMessage',message);
         io.emit('newMessage',generateMessage(message.from,message.text));
         callback('This is from server');
-        // socket.broadcast.emit('newMessage',{
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+    });
+
+    socket.on('createLocationMessage',(coords)=>{
+        io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude,coords.longitude));
     });
 });
 
-app.use(express.static(publicPath));
 
 server.listen(3000, ()=>{
     console.log(`Server is up on ${port}`);
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,25 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage',()=>{
     it('should generate correct message object', ()=>{
     var from = "master";
     var text = 'Some message';
     var message = generateMessage(from,text);
-    var createdAt = message.createdAt;
+    // var createdAt = message.createdAt;
     expect(typeof message.createdAt).toBe('number');
-    expect(message).toMatchObject({from,text,createdAt});
+    expect(message).toMatchObject({from,text});
+    });
+});
+
+describe('generateLocationMessage',()=>{
+    it('should generate correct url',()=>{
+        var from = 'User';
+        var latitude = 11;
+        var longitude = 11;
+        var url = 'https://www.google.com/maps?q=11,11';
+        var message = generateLocationMessage(from,latitude,longitude);
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from,url});
     });
 });
