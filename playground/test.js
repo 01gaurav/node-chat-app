f18aea4c32a4c3da29e0a304243b2294d135551c
master <011999gv@gmail.com>
2020-07-01 04:14:57 +0530

    Add timestamp

---
+++ playground/test.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+var date = moment().valueOf();
+// date.add(2,'year');
+// console.log(date.format('MMM Do, YYYY'));
+console.log(date);
+// console.log(date.format('hh:mm a'));
