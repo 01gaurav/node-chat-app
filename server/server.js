c0355ad286d78db58764bf7140072328c79387c8
master <011999gv@gmail.com>
2020-07-01 03:33:30 +0530

    Add css for chat app

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection',(socket)=>{
     socket.on('createMessage',(message,callback)=>{
         console.log('CreateMessage',message);
         io.emit('newMessage',generateMessage(message.from,message.text));
-        callback('This is from server');
+        callback();
     });
 
     socket.on('createLocationMessage',(coords)=>{
