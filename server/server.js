eb6dba06a2bf5c4d418f0de32f8db3f79b3898f7
master <011999gv@gmail.com>
2020-06-30 07:30:54 +0530

    user joined message

--- server/server.js
+++ server/server.js
@@ -14,14 +14,28 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log('User was Disconnected')
     });
-
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
+    });
     socket.on('createMessage',(message)=>{
         console.log('CreateMessage',message);
-        io.emit('newMessage',{
+        io.emit('newMessage',{ 
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
